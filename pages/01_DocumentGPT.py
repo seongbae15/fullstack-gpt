9b89cc80f10ac97214bd68b8ae1b7eb6ed4a9849
seongbae <pseongbae@gmail.com>
2025-04-02 17:05:55 +0900

    Feat: Add Chat

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,41 @@
 import streamlit as st
+import time
 
-st.write("Document GPT")
+
+MESSAGES = "messages"
+ROLE = "role"
+
+
+st.set_page_config(page_title="DocumentGPT", page_icon="📑")
+st.title("Document GPT")
+
+
+if MESSAGES not in st.session_state:
+    st.session_state[MESSAGES] = []
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state[MESSAGES].append({MESSAGES: message, ROLE: role})
+
+
+for message in st.session_state[MESSAGES]:
+    send_message(
+        message=message[MESSAGES],
+        role=message[ROLE],
+        save=False,
+    )
+
+
+message = st.chat_input("Send a message to the Assistance")
+
+
+if message:
+    send_message(message, "human")
+    time.sleep(1)
+    send_message(f"You said: {message}", "ai")
+
+    with st.sidebar:
+        st.write(st.session_state)
