1977a5d71fdee3850f9f2e9253bc2b02de28fdb7
seongbae <pseongbae@gmail.com>
2025-04-03 17:02:53 +0900

    Delete: commented code

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -53,33 +53,3 @@ if file:
     retriever = embed_file(file)
     s = retriever.invoke("Winston")
     st.write(s)
-
-# if MESSAGES not in st.session_state:
-#     st.session_state[MESSAGES] = []
-
-
-# def send_message(message, role, save=True):
-#     with st.chat_message(role):
-#         st.write(message)
-#     if save:
-#         st.session_state[MESSAGES].append({MESSAGES: message, ROLE: role})
-
-
-# for message in st.session_state[MESSAGES]:
-#     send_message(
-#         message=message[MESSAGES],
-#         role=message[ROLE],
-#         save=False,
-#     )
-
-
-# message = st.chat_input("Send a message to the Assistance")
-
-
-# if message:
-#     send_message(message, "human")
-#     time.sleep(1)
-#     send_message(f"You said: {message}", "ai")
-
-#     with st.sidebar:
-#         st.write(st.session_state)
