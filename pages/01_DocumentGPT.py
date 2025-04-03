057abfed8fed012ba1585e40ab46162f8415f8e7
seongbae <pseongbae@gmail.com>
2025-04-03 17:01:08 +0900

    Feat: Upload file

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS  # FAISS vs Chroma?
+
 import time
 
 
@@ -10,32 +16,70 @@ st.set_page_config(page_title="DocumentGPT", page_icon="📑")
 st.title("Document GPT")
 
 
-if MESSAGES not in st.session_state:
-    st.session_state[MESSAGES] = []
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
 
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state[MESSAGES].append({MESSAGES: message, ROLE: role})
-
-
-for message in st.session_state[MESSAGES]:
-    send_message(
-        message=message[MESSAGES],
-        role=message[ROLE],
-        save=False,
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitters = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
     )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitters)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+
+    return retriever
 
 
-message = st.chat_input("Send a message to the Assistance")
+st.markdown(
+    """
+    Welcome! Use this chatbot to ask questions about your documents.
+"""
+)
+
+file = st.file_uploader(
+    "Upload a .txt, .pdf or .docx file",
+    type=["txt", "pdf", "docx"],
+)
+
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("Winston")
+    st.write(s)
+
+# if MESSAGES not in st.session_state:
+#     st.session_state[MESSAGES] = []
 
 
-if message:
-    send_message(message, "human")
-    time.sleep(1)
-    send_message(f"You said: {message}", "ai")
+# def send_message(message, role, save=True):
+#     with st.chat_message(role):
+#         st.write(message)
+#     if save:
+#         st.session_state[MESSAGES].append({MESSAGES: message, ROLE: role})
 
-    with st.sidebar:
-        st.write(st.session_state)
+
+# for message in st.session_state[MESSAGES]:
+#     send_message(
+#         message=message[MESSAGES],
+#         role=message[ROLE],
+#         save=False,
+#     )
+
+
+# message = st.chat_input("Send a message to the Assistance")
+
+
+# if message:
+#     send_message(message, "human")
+#     time.sleep(1)
+#     send_message(f"You said: {message}", "ai")
+
+#     with st.sidebar:
+#         st.write(st.session_state)
