2eaa3e92b8c7c5c7d86ea67ec2b571cb42ef34dd
seongbae <pseongbae@gmail.com>
2025-04-08 17:38:41 +0900

    Feat: Add uploading file and Load docs using WikipediaRetriever

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,51 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
 
-st.write("Quiz GPT")
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓",
+)
+
+st.title("Quiz GPT")
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitters = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitters)
+    return docs
+
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .txt, .pdf or .docx file",
+            type=["txt", "pdf", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_result=1)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
