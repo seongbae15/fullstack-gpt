69b3e8a6975af345873b13688defa8dacc077433
seongbae <pseongbae@gmail.com>
2025-04-18 00:32:12 +0900

    Feat: AsyncChromiumLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,31 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 
-st.write("site GPT")
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🌐",
+)
+
+html2text_transformer = Html2TextTransformer()
+
+st.markdown(
+    """
+    # SiteGPT
+    Ask questions about the content of a website.
+
+    Start by writing the URL of the website on the sidebar.
+"""
+)
+
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+if url:
+    loader = AsyncChromiumLoader(url)
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
